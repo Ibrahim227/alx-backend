1b74847d2619190e7018a9e9413b591de1499d86
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-23 10:17:17 -0700

    update 0-basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,19 +7,19 @@ class BasicCache(BaseCaching):
     """class BasicCache inherits from BaseCaching"""
     def __init__(self):
         """Instanciate the class"""
-        BaseCaching.__init__(self, cached_data)
-        self.cached_data = {}
+        BaseCaching.__init__(self, cache_data)
+        self.cache_data = {}
 
     def put(self, key, item):
         """Must assign to the dictionary self.cache_data"""
         if key or item is None:
             pass
         else:
-            self.cached_data[key] = item
+            self.cache_data[key] = item
 
-    def get(self, key);
+    def get(self, key):
         """Must return the value in self.cache_data linked to key"""
-        if key is None or key not in self.cached_data:
+        if key is None or key not in self.cache_data:
             return None
         else:
-            return self.cached_data[key]
+            return self.cache_data[key]
