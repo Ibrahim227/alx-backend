cefe44656f82ffa7f0053b39d1a57650fc17edf2
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-24 08:42:09 -0700

    upadte 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,19 +7,16 @@ class BasicCache(BaseCaching):
     """class BasicCache inherits from BaseCaching"""
     def __init__(self):
         """Instanciate the class"""
-        BaseCaching.__init__(self, cache_data)
-        self.cache_data = {}
+        super().__init__()
 
     def put(self, key, item):
         """Must assign to the dictionary self.cache_data"""
         if key or item is None:
             pass
-        else:
-            self.cache_data[key] = item
+        self.cache_data[key] = item
 
     def get(self, key):
         """Must return the value in self.cache_data linked to key"""
         if key is None or key not in self.cache_data:
             return None
-        else:
-            return self.cache_data[key]
+        return self.cache_data[key]
