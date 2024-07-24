b1da58310abfcb30b1ef2662c88ff91fcaaa41f9
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-24 16:47:32 -0700

    update 1-fifo-cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,17 +8,19 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Instantiate the class"""
         super().__init__()
+        self.cache_keys = []
 
     def put(self, key, item):
         """put method"""
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            popItem = next(iter(self.cache_data))
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            popItem = self.cache_keys.pop(0)
+            del self.cache_data[popItem]
             print("DISCARD: {}".format(popItem))
-            # self.cache_data.popitem()
 
+        self.cache_keys.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
