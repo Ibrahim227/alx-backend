ab47043e6c977250d8a1f64789d8f9eac9c72eb1
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-24 15:23:57 -0700

    update 3-lru_cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Import Module/Lib"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Class LIFOCache, inherits from BaseCaching"""
+    def __init__(self):
+        """initalize the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Put method"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            popItem = self.cache_data.popitem()
+            print("DISCARD: {}".format(popItem))
+            # self.cache_data.popitem()
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get method"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
