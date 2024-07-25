f343eb0a0b3f7c3b849620c45bd22b1f93c910a2
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-24 17:11:23 -0700

    update 2-lifo_cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -8,17 +8,19 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """initalize the class"""
         super().__init__()
+        self.cache_keys = []
 
     def put(self, key, item):
         """Put method"""
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            popItem = self.cache_data.popitem()
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            popItem = self.cache_keys.pop()
+            del self.cache_data[popItem]
             print("DISCARD: {}".format(popItem))
-            # self.cache_data.popitem()
 
+        self.cache_keys.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
