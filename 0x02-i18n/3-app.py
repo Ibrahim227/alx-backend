86140c69be7b196110ca835011aa24366726356b
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-30 16:37:55 -0700

    update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,25 +1,30 @@
-#!/usr/bin/rnv python3
-"""Import required MOdu;e/Lib"""
-from flask import Flask, render_template, request
-fromflask_babel import Babel, _
+#!/usr/bin/env python3
+"""Import required Modul'Lib"""
+from flask import Flask, render_template
+from flask_babel import Babel
 
 
-app =Flask(__name__)
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
-@label.localeselector
+@label.selector
 def get_locale():
-    """REturn locale"""
-    return request.args.get('lang', 'en')
+    """selct and return lang"""
+    return request.accept_languages.best_match(['en', 'de', 'fr'])
 
 @app.route('/')
-def index():
-    """return index"""
-    return render_template('3-index.html')
+def index() -> str:
+    """ return 1-index.html"""
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
