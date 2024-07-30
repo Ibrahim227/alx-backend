8dad4afc02e52a6d5f4da99a669c22e0970f5ca9
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-30 16:33:39 -0700

    update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,26 +4,25 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
-babel = Babel(app)
+# app = Flask(__name__)
+# app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+# app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+# babel = Babel(app)
 
 
 class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
-
-    def __init__(self):
-        """Initialize the class"""
-        pass
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
-
+babel = Babel(app)
 
 @app.route('/')
-def index():
+def index() -> str:
     """ return 1-index.html"""
     return render_template('1-index.html')
 
